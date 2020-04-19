95a36134ff56df6c9f764175504dc4aba04ed24e
Shub4am <shub4am@gmail.com>
2020-04-19 07:00:42 +0530

    Updates

---
+++ precache-manifest.d028ff7bfcfd77f084410f862bcc5fff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eda877e4b1369b341c86486fc4f96eb",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "fc2b0b8eae687f46fdea",
+    "url": "/calculator/static/css/main.fbbcc050.chunk.css"
+  },
+  {
+    "revision": "ae692845c2ccbde5faa7",
+    "url": "/calculator/static/js/2.8070d701.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator/static/js/2.8070d701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc2b0b8eae687f46fdea",
+    "url": "/calculator/static/js/main.3b2cefaa.chunk.js"
+  },
+  {
+    "revision": "4c4aaba088808e79c8e8",
+    "url": "/calculator/static/js/runtime-main.1846f5a0.js"
+  }
+]);
